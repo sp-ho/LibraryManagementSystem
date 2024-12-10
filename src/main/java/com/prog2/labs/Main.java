387b2449c80167fc9262aac676b6d13f0c4b073b
sp-ho <106715796+sp-ho@users.noreply.github.com>
2024-12-09 20:38:18 -0500

    Update Main.java

--- src/main/java/com/prog2/labs/Main.java
+++ src/main/java/com/prog2/labs/Main.java
@@ -4,29 +4,21 @@ import java.awt.event.ActionEvent;
 
 public class Main {
 
-	/**
-	 * Write your test code below in the main (optional).
-	 *
-	 */
 	public static void main(String[] args) {
 
 		AdminLogin.frame = new AdminLogin();
 		AdminLogin.frame.setVisible(true);
-//		if (login.rdbtnLibrarian.isSelected() && login.btnLogin.action(ActionEvent, login)); // LibrarianRadio selected when login pressed
-		// AdminMain view = new AdminMain();
-		// Librarian modelL = new Librarian();
-		// LibrarianController controller = new LibrarianController(modelL, null, null, view);
-		// login.setVisible(false);
-		// view.setVisible(true);
-		// if (addBooks button is clicked)
-		//		AddBook addBook = new AddBook();
-		//		addBook.setVisible(true);
-		// 		Book book = new Book(dsf, sdf, sdf)
-		//      controller.addBook(book, "books") 
+		if (login.rdbtnLibrarian.isSelected() && login.btnLogin.action(ActionEvent, login)); // LibrarianRadio selected when login pressed
+		AdminMain view = new AdminMain();
+		Librarian modelL = new Librarian();
+		LibrarianController controller = new LibrarianController(modelL, null, null, view);
+		login.setVisible(false);
+		view.setVisible(true);
+		if (addBooks button is clicked)
+				AddBook addBook = new AddBook();
+				addBook.setVisible(true);
+				Book book = new Book(dsf, sdf, sdf)
+		     controller.addBook(book, "books") 
 	}
-
-	/**
-	 * Please refer to he README file for question(s) description
-	 */
 
 }
